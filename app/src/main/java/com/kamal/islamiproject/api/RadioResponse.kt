a1b9a36776b3da1e7d191094f049ba116b940177
ahmedkamal22 <ak0850000@gmail.com>
2021-05-08 05:02:13 +0300

    RadioResponse

---
+++ app/src/main/java/com/kamal/islamiproject/api/RadioResponse.kt
@@ -0,0 +1,18 @@
+package com.kamal.islamiproject.api
+
+import com.google.gson.annotations.SerializedName
+
+data class RadioResponse(
+
+	@field:SerializedName("radios")
+	val radios: List<RadiosChannel?>? = null
+)
+
+data class RadiosChannel(
+
+	@field:SerializedName("name")
+	val name: String? = null,
+
+	@field:SerializedName("radio_url")
+	val radioUrl: String? = null
+)
